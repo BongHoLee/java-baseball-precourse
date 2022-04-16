0f9e86317e4eaa68caea6bd7075ef74b034e6f7f
bongholee <leebongho9204@gmail.com>
2022-04-16 20:55:59 +0900

    test(StrikeZoneFactoryTest): convert test method implementation

--- src/test/java/baseball/game/referee/StrikeZoneFactoryTest.java
+++ src/test/java/baseball/game/referee/StrikeZoneFactoryTest.java
@@ -10,32 +10,36 @@ import org.junit.jupiter.api.RepeatedTest;
 
 public class StrikeZoneFactoryTest {
 
-    private int strikeZone;
+    private int[] strikeZone;
 
     @BeforeEach
     void setUp() {
         strikeZone = StrikeZoneFactory.createStrikeZone();
     }
 
-    @DisplayName("생성된 strikeZone이 123 ~ 987 사이의 수로 구성되는지 테스트")
     @RepeatedTest(100)
+    @DisplayName("생성된 strikeZone이 123 ~ 987 사이의 수로 구성되는지 테스트")
     public void check_strikeZone_is_between_123_and_987() {
-        assertThat(strikeZone).isBetween(123, 987);
+        int strikeZoneNumber = 0;
+        int digit = 100;
+        for (int eachStrike : strikeZone) {
+            strikeZoneNumber += eachStrike * digit;
+            digit = digit/10;
+        }
+
+        assertThat(strikeZoneNumber).isBetween(123, 987);
     }
 
-    @DisplayName("생성된 stirkeZone에 중복된 수가 존재하지 않는지 테스트")
     @RepeatedTest(100)
+    @DisplayName("생성된 stirkeZone에 중복된 수가 존재하지 않는지 테스트")
     public void check_strikeZone_is_not_duplicated() {
         Set<Integer> strikeSet = new HashSet<>();
 
-        int count = 0;
-        while ((strikeZone%10) > 0) {
-            strikeSet.add(strikeZone % 10);
-            strikeZone = strikeZone / 10;
-            count ++;
+        for (int eachStrike : strikeZone) {
+            strikeSet.add(eachStrike);
         }
 
-        assertThat(strikeSet.size()).isEqualTo(count);
+        assertThat(strikeSet.size()).isEqualTo(strikeZone.length);
     }
 
 }
