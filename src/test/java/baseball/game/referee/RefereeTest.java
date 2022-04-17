2702fa3cd037bf49b79ccb96439e5c48bdd0539d
bongholee <leebongho9204@gmail.com>
2022-04-17 19:39:48 +0900

    test(RefereeTest): simplify RefereeTest result creation code
    refactoring contents
    - simplify result creation code

--- src/test/java/baseball/game/referee/RefereeTest.java
+++ src/test/java/baseball/game/referee/RefereeTest.java
@@ -36,12 +36,12 @@ public class RefereeTest {
 
     static Stream<Arguments> validateParameters() {
         return Stream.of(
-                Arguments.of(new int[] {1, 2, 3}, new int[] {1, 2, 3}, "3스트라이크", "스트라이크존 123, 피쳐볼 123일 때 3스트라이크" ),
-                Arguments.of(new int[] {1, 2, 3}, new int[] {1, 2, 5}, "2스트라이크", "스트라이크존 123, 피쳐볼 125일 때 2스트라이크" ),
-                Arguments.of(new int[] {2, 7, 5}, new int[] {2, 5, 7}, "2볼 1스트라이크", "스트라이크존 275, 피쳐볼 257 때 2볼 1스트라이크" ),
-                Arguments.of(new int[] {1, 2, 3}, new int[] {4, 5, 6}, "낫싱", "스트라이크존 123, 피쳐볼 456 때 낫싱" ),
-                Arguments.of(new int[] {8, 7, 3}, new int[] {8, 1, 7}, "1볼 1스트라이크", "스트라이크존 873, 피쳐볼 817 때 1볼 1스트라이크" ),
-                Arguments.of(new int[] {5, 4, 9}, new int[] {9, 5, 4}, "3볼", "스트라이크존 549, 피쳐볼 954 때 3볼" )
+                Arguments.of(new int[] {1, 2, 3}, new int[] {1, 2, 3}, "0B 3S", "스트라이크존 123, 피쳐볼 123일 때 [0B 3S]" ),
+                Arguments.of(new int[] {1, 2, 3}, new int[] {1, 2, 5}, "0B 2S", "스트라이크존 123, 피쳐볼 125일 때 [0B 2S]" ),
+                Arguments.of(new int[] {2, 7, 5}, new int[] {2, 5, 7}, "2B 1S", "스트라이크존 275, 피쳐볼 257일 때 [2B 1S]" ),
+                Arguments.of(new int[] {1, 2, 3}, new int[] {4, 5, 6}, "0B 0S", "스트라이크존 123, 피쳐볼 456일 때 [0B 0S]" ),
+                Arguments.of(new int[] {8, 7, 3}, new int[] {8, 1, 7}, "1B 1S", "스트라이크존 873, 피쳐볼 817일 때 [1B 1S]" ),
+                Arguments.of(new int[] {5, 4, 9}, new int[] {9, 5, 4}, "3B 0S", "스트라이크존 549, 피쳐볼 954일 때 [3B 0S]" )
         );
     }
 
@@ -49,7 +49,7 @@ public class RefereeTest {
         setStrikeZone(strikeZone);
         setPitchNumbers(pitchNumbers);
 
-        return printResult();
+        return result();
     }
 
     private void setStrikeZone(int[] strikeZone) throws Exception{
@@ -62,66 +62,9 @@ public class RefereeTest {
         when(pitcher.pitch()).thenReturn(pitchNumbers);
     }
 
-    private String printResult() {
-        StringBuilder result = new StringBuilder();
+    private String result() {
         Map<ScoreStatus, Integer> scoreStatusMap = referee.playBall();
 
-        return buildResult(scoreStatusMap);
+        return scoreStatusMap.get(ScoreStatus.BALL) + "B " + scoreStatusMap.get(ScoreStatus.STRIKE) + "S";
     }
-
-    private String buildResult(Map<ScoreStatus, Integer> scoreStatusMap) {
-        if (isNothing(scoreStatusMap)) {
-            return buildNothingStatus();
-        }
-
-        return scored(scoreStatusMap);
-    }
-
-    private boolean isNothing(Map<ScoreStatus, Integer> scoreStatusMap) {
-        return scoreStatusMap.get(ScoreStatus.STRIKE) == 0 &&
-                scoreStatusMap.get(ScoreStatus.BALL) == 0;
-    }
-
-    private String scored(Map<ScoreStatus, Integer> scoreStatusMap) {
-        return buildBallStatus(scoreStatusMap)
-                .append(space(scoreStatusMap))
-                .append(buildStrikeStatus(scoreStatusMap))
-                .toString();
-    }
-
-    private String buildNothingStatus() {
-        return "낫싱";
-    }
-
-    private StringBuilder buildStrikeStatus(Map<ScoreStatus, Integer> scoreStatusMap) {
-        StringBuilder strikeStatus = new StringBuilder();
-        if (scoreStatusMap.get(ScoreStatus.STRIKE) != 0) {
-            strikeStatus
-                    .append((scoreStatusMap.get(ScoreStatus.STRIKE)).toString())
-                    .append(ScoreStatus.STRIKE);
-        }
-
-        return strikeStatus;
-    }
-
-    private StringBuilder buildBallStatus(Map<ScoreStatus, Integer> scoreStatusMap) {
-        StringBuilder ballStatus = new StringBuilder();
-        if (scoreStatusMap.get(ScoreStatus.BALL) != 0) {
-            ballStatus
-                    .append((scoreStatusMap.get(ScoreStatus.BALL)).toString())
-                    .append(ScoreStatus.BALL);
-        }
-
-        return ballStatus;
-    }
-
-    private StringBuilder space(Map<ScoreStatus, Integer> scoreStatusMap) {
-        StringBuilder spaceStr = new StringBuilder();
-        if (scoreStatusMap.get(ScoreStatus.BALL) != 0 && scoreStatusMap.get(ScoreStatus.STRIKE) != 0) {
-            spaceStr.append(" ");
-        }
-
-        return spaceStr;
-    }
-
 }
